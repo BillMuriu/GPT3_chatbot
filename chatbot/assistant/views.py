d9e98383582618922ad16fb90c339c16370e5206
unknown <njorogekamau63@gmail.com>
2023-03-12 14:03:24 +0300

    adjusting the views

--- chatbot/assistant/views.py
+++ chatbot/assistant/views.py
@@ -1,13 +1,9 @@
 from django.shortcuts import render
-# import HttpResponse from django.urls
-from django.http import HttpResponse
-
 
 # this is the home view for handling home page logic
 def home(request):
-    return HttpResponse('The Home Page')
-
+    return render(request, 'assistant/home.html')
 
 # this is the view for handling errors
 def error_handler(request):
-    return HttpResponse('404 Page')
+    return render(request, 'assistant/404.html')
