d4d775accd982deb1967f711abc18e81f7033e6a
unknown <njorogekamau63@gmail.com>
2023-03-12 12:53:53 +0300

    Iteration

--- script.js
+++ script.js
@@ -62,50 +62,50 @@ chatboxForm.addEventListener('submit', function (e) {
 
 
 
-function addZero(num) {
-	return num < 10 ? '0'+num : num
-}
+// function addZero(num) {
+// 	return num < 10 ? '0'+num : num
+// }
 
-function writeMessage() {
-	const today = new Date()
-	let message = `
-		<div class="chatbox-message-item sent">
-			<span class="chatbox-message-item-text">
-				${textarea.value.trim().replace(/\n/g, '<br>\n')}
-			</span>
-			<span class="chatbox-message-item-time">${addZero(today.getHours())}:${addZero(today.getMinutes())}</span>
-		</div>
-	`
-	chatboxMessageWrapper.insertAdjacentHTML('beforeend', message)
-	chatboxForm.style.alignItems = 'center'
-	textarea.rows = 1
-	textarea.focus()
-	textarea.value = ''
-	chatboxNoMessage.style.display = 'none'
-	scrollBottom()
-}
+// function writeMessage() {
+// 	const today = new Date()
+// 	let message = `
+// 		<div class="chatbox-message-item sent">
+// 			<span class="chatbox-message-item-text">
+// 				${textarea.value.trim().replace(/\n/g, '<br>\n')}
+// 			</span>
+// 			<span class="chatbox-message-item-time">${addZero(today.getHours())}:${addZero(today.getMinutes())}</span>
+// 		</div>
+// 	`
+// 	chatboxMessageWrapper.insertAdjacentHTML('beforeend', message)
+// 	chatboxForm.style.alignItems = 'center'
+// 	textarea.rows = 1
+// 	textarea.focus()
+// 	textarea.value = ''
+// 	chatboxNoMessage.style.display = 'none'
+// 	scrollBottom()
+// }
 
-function autoReply() {
-	const today = new Date()
-	let message = `
-		<div class="chatbox-message-item received">
-			<span class="chatbox-message-item-text">
-				Thank you for your awesome support!
-			</span>
-			<span class="chatbox-message-item-time">${addZero(today.getHours())}:${addZero(today.getMinutes())}</span>
-		</div>
-	`
-	chatboxMessageWrapper.insertAdjacentHTML('beforeend', message)
-	scrollBottom()
-}
+// function autoReply() {
+// 	const today = new Date()
+// 	let message = `
+// 		<div class="chatbox-message-item received">
+// 			<span class="chatbox-message-item-text">
+// 				Thank you for your awesome support!
+// 			</span>
+// 			<span class="chatbox-message-item-time">${addZero(today.getHours())}:${addZero(today.getMinutes())}</span>
+// 		</div>
+// 	`
+// 	chatboxMessageWrapper.insertAdjacentHTML('beforeend', message)
+// 	scrollBottom()
+// }
 
-function scrollBottom() {
-	chatboxMessageWrapper.scrollTo(0, chatboxMessageWrapper.scrollHeight)
-}
+// function scrollBottom() {
+// 	chatboxMessageWrapper.scrollTo(0, chatboxMessageWrapper.scrollHeight)
+// }
 
-function isValid(value) {
-	let text = value.replace(/\n/g, '')
-	text = text.replace(/\s/g, '')
+// function isValid(value) {
+// 	let text = value.replace(/\n/g, '')
+// 	text = text.replace(/\s/g, '')
 
-	return text.length > 0
-}
+// 	return text.length > 0
+// }
